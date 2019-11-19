8c807fd6d63cbe263aeac131cf3a4b1b90eb1c67
Manik <mnk_400@icloud.com>
2019-11-19 16:43:56 -0500

    Uploading everything

--- shaperec.m
+++ shaperec.m
@@ -4,36 +4,37 @@ T=zeros(3,9);
  for h=1:9
     switch h
        case 1
-          Img = imread('t1.bmp');
+          Img = imread('triangle1.bmp');
           Target=[1;-1;-1] ;
-       case 2 
-          Img = imread('t2.bmp');
+       case 2
+          Img = imread('triangle2.bmp');
           Target=[1;-1;-1] ;
        case 3
-           Img = imread('t3.bmp');
+           Img = imread('triangle3.bmp');
            Target=[1;-1;-1] ;
-       case 4
-           Img = imread('r1.bmp');
+       case 4w
+        ` 1 `
+           Img = imread('rect1.bmp');
            Target=[-1;1;-1] ;
        case 5
-           Img = imread('r2.bmp');
+           Img = imread('rect2.bmp');
            Target=[-1;1;-1];
        case 6
-           Img = imread('r3.bmp');
+           Img = imread('rect3.bmp');
            Target=[-1;1;-1];
        case 7
-           Img = imread('c1.bmp');
+           Img = imread('circle1.bmp');
            Target=[-1;-1;1];
        case 8
-           Img = imread('c2.bmp');
+           Img = imread('circle2.bmp');
            Target=[-1;-1;1];
        case 9
-           Img = imread('c3.bmp');
+           Img = imread('circle3.bmp');
            Target=[-1;-1;1];
     end
-    
+
     T(:,h)=Target ;
- 
+
 [Num_Row,Num_column] = size(Img) ;
    for i=1:Num_Inputs
        for j=(((Num_Row/Num_Inputs)*(i-1))+1) : ((Num_Row/Num_Inputs)*(i))
@@ -46,20 +47,20 @@ T=zeros(3,9);
    end
  end
 S=zeros(Num_Inputs,3);
- 
+
 for h=1:3
     switch h
        case 1
-          Img = imread('t4.bmp');
-          
+          Img = imread('triangle4.bmp');
+
        case 2
-           Img = imread('r4.bmp');
-           
+           Img = imread('rect4.bmp');
+
         case 3
-           Img = imread('c4.bmp');
-           
-    end 
- 
+           Img = imread('circle4.bmp');
+
+    end
+
 [Num_Row,Num_column] = size(Img) ;
 
   for i=1:Num_Inputs
@@ -71,22 +72,22 @@ for h=1:3
            end
        end
    end
-end 
+end
 S=zeros(Num_Inputs,3);
- 
+
 for h=1:3
     switch h
        case 1
-          Img = imread('t4.bmp');
-          
+          Img = imread('triangle4.bmp');
+
        case 2
-           Img = imread('r4.bmp');
-           
+           Img = imread('rect4.bmp');
+
         case 3
-           Img = imread('c4.bmp');
-           
-    end 
- 
+           Img = imread('circle4.bmp');
+
+    end
+
 [Num_Row,Num_column] = size(Img) ;
 
   for i=1:Num_Inputs
@@ -98,22 +99,22 @@ for h=1:3
            end
        end
    end
-end 
+end
 S=zeros(Num_Inputs,3);
- 
+
 for h=1:3
     switch h
        case 1
-          Img = imread('t4.bmp');
-          
+          Img = imread('triangle4.bmp');
+
        case 2
-           Img = imread('r4.bmp');
-           
+           Img = imread('rect4.bmp');
+
         case 3
-           Img = imread('c4.bmp');
-           
-    end 
- 
+           Img = imread('circle4.bmp');
+
+    end
+
 [Num_Row,Num_column] = size(Img) ;
 
   for i=1:Num_Inputs
@@ -125,4 +126,4 @@ for h=1:3
            end
        end
    end
-end 
+end
